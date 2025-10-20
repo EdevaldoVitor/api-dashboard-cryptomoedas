f7108120a223ce303ad5478213bd4403f5e6214f
EdevaldoVitor <vitor.cohene@hotmail.com>
2025-10-20 11:50:25 -0300

    Commit inicial - Dashboard de Criptomoedas

---
+++ src/main/java/com/api/crypto/dashboard/ApiDashboardCryptomoedasApplication.java
@@ -0,0 +1,13 @@
+package com.api.crypto.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDashboardCryptomoedasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDashboardCryptomoedasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/crypto/dashboard/ApiDashboardCryptomoedasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.crypto.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDashboardCryptomoedasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
